df1d6d7c9a0a58a981dd833dc679586d7356bea4
Rahul Gupta <guptarahul97480@gmail.com>
2021-06-02 22:33:24 +0530

    Optimized

--- HitApi.php
+++ HitApi.php
@@ -1,5 +1,6 @@
+<head><meta content='10' http-equiv='refresh'/></head>
 <?php
-function callAPI($method, $url, $data){
+function callAPI($method, $url){
    $curl = curl_init();
    curl_setopt($curl, CURLOPT_URL, $url);
    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -9,37 +10,61 @@ function callAPI($method, $url, $data){
    return $result;
 }
 
-$result = callAPI('GET', 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=711204&date='. date("d-m-Y"), false);
-$json = json_decode($result, true);
 
-for($i=0;$i<=10;$i++){
-   if(array_key_exists($i,$json['centers'])){
-      if($json['centers'][$i]['sessions'][0]['min_age_limit'] == 45){ //change to 18
-         if($json['centers'][$i]['sessions'][0]['available_capacity'] == 0 ){ //change to greate than 0 
-            echo "<h1><centre>Vacccine Available in Hospital " . $json['centers'][$i]['name'] . " at " . $json['centers'][$i]['address'] . " vaccine " .  $json['centers'][$i]['sessions'][0]['vaccine'] ;
-            echo "<audio id='audioBox'>
-            <source src='song.wav' type='audio/wav'/>
-         </audio>";
-         echo "<script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
-         <script>
-            setInterval(function(){
-            load();
-         },3000);
-         function load(){
-            jQuery.ajax({
-               url:'get.php',
-               success:function(result){
-                  var data=jQuery.parseJSON(result);
-                  if(data.sound=='yes'){
-                     jQuery('#audioBox')[0].play();
-                  }
-               }
-            });
-         }</script>";
-         }
+function startSound($json){
+   $size = sizeof($json['centers']);
+   for($i=0;$i<$size;$i++){
+      $size_session = sizeof($json['centers'][$i]['sessions']);
+      for($j=0;$j<$size_session;$j++){
+          if($json['centers'][$i]['sessions'][$j]['min_age_limit'] == 18){
+            if($json['centers'][$i]['sessions'][$j]['available_capacity_dose1'] > 0 ){
+               echo "<center><h3>Vacccine Available in Hospital " . $json['centers'][$i]['name'] . " at " . $json['centers'][$i]['address'] . " vaccine " .  $json['centers'][$i]['sessions'][$j]['vaccine'] . "  DATE  ". $json['centers'][$i]['sessions'][$j]['date'] ."</h3></center>" ;
+
+               $con=mysqli_connect('localhost','root','','Vaccine');
+
+               $center_name =  $json['centers'][$i]['name'];
+               $address =  $json['centers'][$i]['address'];
+               $vaccine =  $json['centers'][$i]['sessions'][$j]['vaccine'];
+               $age = $json['centers'][$i]['sessions'][$j]['min_age_limit'];
+               $vaccine_date = $json['centers'][$i]['sessions'][$j]['date'];
+               date_default_timezone_set("Asia/Kolkata");
+               $date = date("Y-m-d h:i:sa");
+
+               $con=mysqli_connect('localhost','root','','Vaccine');
+
+               $sql = "INSERT INTO info (center_name, address, vaccine, age, vaccine_date, exact_time) VALUES ('$center_name', '$address', '$vaccine', '$age', '$vaccine_date','$date')";
+               $res=mysqli_query($con,$sql);
+               
+
+
+               echo "<audio id='audioBox'><source src='song.wav' type='audio/wav'/></audio>";
+               echo "<script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
+                     <script>
+                        setInterval(function(){
+                        load();
+                     },3000);
+                     function load(){
+                        jQuery.ajax({
+                           url:'get.php',
+                           success:function(result){
+                              var data=jQuery.parseJSON(result);
+                              if(data.sound=='yes'){
+                                 jQuery('#audioBox')[0].play();
+                              }
+                           }
+                        });
+                     }</script>";
+
+            }
+          }
       }
-   }
+   } 
 }
 
+$result1 = callAPI('GET', 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=721&date='.date("d-m-y"));
+$json = json_decode($result1, true);
+// echo "<pre>";
+// print_r($json);
+startSound($json);
 
 ?>
